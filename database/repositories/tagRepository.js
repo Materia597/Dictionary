70c89c66f3584631fc3e3a87c73172ea94bf4743
Materia597 <112108517+Materia597@users.noreply.github.com>
2026-05-25 18:01:53 -0700

    Moved erroneous tag services to the tag repository file and crated wrappers for the methods
    This is done as a layer of abstraction, since direct database access should be delegated to the repositories that is then called and validated from the services.

---
+++ database/repositories/tagRepository.js
@@ -0,0 +1,63 @@
+
+const db = require('../db')
+
+const createTagStatement = db.prepare(`
+    INSERT INTO tags (name) VALUES (:name)
+`)
+
+const getTagStatement = db.prepare(`
+    SELECT * FROM tags WHERE name = :name
+`)
+
+const deleteTagStatement = db.prepare(`
+    DELETE FROM tags WHERE name = :name
+`)
+
+const recentTagsStatement = db.prepare(`
+    SELECT * FROM tags ORDER BY id DESC LIMIT 10    
+`)
+
+/**
+ * Creates a new tag in the tags database.
+ * 
+ * @param {string} tagName Name of the new tag
+ * @throws Error: UNIQUE constraint failed
+ */
+const createTag = (tagName) => {
+    return createTagStatement.run({name: tagName.toLowerCase().trim()});
+}
+
+/**
+ * Gets a tag from the tags database
+ * 
+ * @param {string} tagName 
+ */
+const getTag = (tagName) => {
+    return getTagStatement.get({name: tagName.toLowerCase().trim()});
+}
+
+
+/**
+ * Deletes a tag from the tags database
+ * 
+ * @param {string} tagName 
+ * @returns 
+ */
+const deleteTag = (tagName) => {
+    return deleteTagStatement.run({name: tagName.toLowerCase().trim()});
+}
+
+/**
+ * Gets the 10 most recent tags from the tag database
+ * as an array.
+ * 
+ * @returns 
+ */
+const recentTags = () => {
+    return recentTagsStatement.all();
+}
+
+module.exports.createTag = createTag;
+module.exports.getTag = getTag;
+module.exports.deleteTag = deleteTag;
+module.exports.recentTags = recentTags;
--- main/services/tagServices.js
+++ main/services/tagServices.js
@@ -1,62 +1,25 @@
 
-const db = require('../../database/db')
+const tagRepo = require('../../database/repositories/tagRepository')
 
 
-const createTagStatement = db.prepare(`
-    INSERT INTO tags (name) VALUES (:name)
-`)
-
-const getTagStatement = db.prepare(`
-    SELECT * FROM tags WHERE name = :name
-`)
-
-const deleteTagStatement = db.prepare(`
-    DELETE FROM tags WHERE name = :name
-`)
-
-const recentTagsStatement = db.prepare(`
-    SELECT * FROM tags ORDER BY id DESC LIMIT 10    
-`)
-
-/**
- * Creates a new tag in the tags database.
- * 
- * @param {string} tagName Name of the new tag
- * @throws Error: UNIQUE constraint failed
- */
 const createTag = (tagName) => {
-    return createTagStatement.run({name: tagName.toLowerCase().trim()});
+    return tagRepo.createTag(tagName)
 }
 
-/**
- * Gets a tag from the tags database
- * 
- * @param {string} tagName 
- */
 const getTag = (tagName) => {
-    return getTagStatement.get({name: tagName.toLowerCase().trim()});
+    return tagRepo.getTag(tagName)
 }
 
-
-/**
- * Deletes a tag from the tags database
- * 
- * @param {string} tagName 
- * @returns 
- */
 const deleteTag = (tagName) => {
-    return deleteTagStatement.run({name: tagName.toLowerCase().trim()});
+    return tagRepo.deleteTag(tagName)
 }
 
-/**
- * Gets the 10 most recent tags from the tag database
- * as an array.
- * 
- * @returns 
- */
 const recentTags = () => {
-    return recentTagsStatement.all();
+    return tagRepo.recentTags();
 }
+
+
+
 
 module.exports.createTag = createTag;
 module.exports.getTag = getTag;
