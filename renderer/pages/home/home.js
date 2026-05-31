aa22b4792ef534ee7971cd6243e2fe4c42cb944b
Materia597 <112108517+Materia597@users.noreply.github.com>
2026-05-30 18:13:31 -0700

    The home page will not automatically display the most recent created entries.

--- renderer/pages/home/home.js
+++ renderer/pages/home/home.js
@@ -1,2 +1,34 @@
+const definitionsPreview = document.getElementById('recent-definitions-preview')
+
+/**
+ * 
+ * @param {number} entryId 
+ * @param {string} term 
+ * @param {string} definition 
+ * @param {string[]} tags 
+ */
+const makePreview = (entryId, term, definition, tags) => {
+    let tagText = tags.map(t => `<span class="def-tag">${t.name}</span>`).join("\n")
+
+    return `
+        <article class="dict-entry-preview" data-entryId="${entryId}">
+            <h2 class="term-heading">${term}</h2>
+            <span class="definition-tags">
+                ${tagText}
+            </span>
+            <p class="definition-preview">${definition}</p>
+        </article>
+    `
+}
 
 
+
+const populateRecent = async () => {
+    const results = await window.entries.recentEntries()
+    
+    results.forEach(r => {
+        definitionsPreview.insertAdjacentHTML('beforeend', makePreview(r.id, r.term, r.definition, r.tags))
+    }) 
+}
+
+populateRecent()
